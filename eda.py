1c86fbfdfe38d95e5f6d47a26b15a6c0222f1dad
Dhruv Kapadia <dhruvkapadia20@gmail.com>
2026-07-07 23:16:56 -0600

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,7 +1,13 @@
-# %% cell 1: Loading the data
+# %% Cell 1: Loading the data
 import pandas as pd
-
 train = pd.read_csv("data/train.csv")
 test = pd.read_csv("data/test.csv")
-
 train.head()
+
+# %%
+train.info()
+# %%
+train.isnull().sum()
+# %%
+train['target'].value_counts(normalize=True)*100
+# %%
